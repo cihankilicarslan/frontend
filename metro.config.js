1b606ca2a6f1e1a9935a50a143c1fd2d9a260f93
Cihan Kilicarslan <cihankilicarslan@hotmail.com>
2024-05-25 12:36:28 -0400

    first

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,10 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig();
+  return {
+    resolver: {
+      assetExts: [...defaultConfig.resolver.assetExts, 'db'],
+    },
+  };
+})();
